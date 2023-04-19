89be08095dea48234f920d6a21c2d85f5e9e6e28
unknown <timur.dev@mail.ru>
2023-04-20 02:16:57 +0300

    refactor(extended-2.1): change JS according to new classes

--- app.js
+++ app.js
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className='main__list-item';
+    checkBox.className='main__list-item__task-checkbox';
     label.innerText=taskString;
-    label.className='task';
+    label.className='task main__list-item__label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className='delete-icon';
+    deleteButtonImg.alt='delete-icon';
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -85,11 +89,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
+    var containsClass=listItem.classList.contains("main__list-item_edit");
+    //If class of the parent is .main__list-item_edit
     if(containsClass){
 
-        //switch to .editmode
+        //switch to .main__list-item_edit
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +102,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    //toggle .main__list-item_edit on the parent.
+    listItem.classList.toggle("main__list-item_edit");
 };
 
 
